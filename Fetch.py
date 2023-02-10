3e67ac1720d2daa8c738c9505bd76faa749cf72f
spellsharp <svcodin22@gmail.com>
2023-02-10 10:04:59 +0530

    created .gitignore

--- Fetch.py
+++ Fetch.py
@@ -1,7 +1,7 @@
 import requests
 import os
 from PIL import Image as img
-
+import api
 photos = []
 
 def Image(nameRover='curiosity', solYear='1000', cameraName='fhaz', earthDate='2015-6-3', img_width=850, img_height=850):
@@ -14,15 +14,9 @@ def Image(nameRover='curiosity', solYear='1000', cameraName='fhaz', earthDate='2
         global numPhotos
         os.makedirs('marsImages',exist_ok=True)
         path = os.path.join(os.getcwd(),'marsImages')
-
-        # stRE2d9R0wIxVfFzV05MUjqJ1m3k3AJ2pOw6nx1W
-        # MyG8gwSTFPwax20aBofVEvR1mP2hwCgW8l60RM0C
-        # 07OpzC4a1zoNdt0PDDPnoDJOJbA6EETPbAbl8WsZ
-
-        api_key = '07OpzC4a1zoNdt0PDDPnoDJOJbA6EETPbAbl8WsZ'
         
-        res = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{nameRover}/photos?sol={solYear}&camera={cameraName}&?earth_date={earthDate}&api_key={api_key}").json()
-        # print(res)
+        res = requests.get(f"https://api.nasa.gov/mars-photos/api/v1/rovers/{nameRover}/photos?sol={solYear}&camera={cameraName}&?earth_date={earthDate}&api_key={api.api_key1}").json()
+
         numPhotos = len(res['photos'])
         print()
         print("Number Of Photos: ", numPhotos)
